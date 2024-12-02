3d278e307efab187ff8570f94326d525cbd15589
Artur <artur@example.com>
2024-12-02 17:23:53 +0300

    portfolio

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,4 @@
-﻿
-smile_footprint = '\U0001F463'
-
+﻿smile_footprint = '\U0001F463'
 user_name = 'David'
 user_age = '14'
 result = (f'My name is {user_name} , I am {user_age} years old {smile_footprint}')
