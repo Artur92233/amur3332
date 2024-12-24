a00eb669eed37a222b6b681951936a675a0aaf11
Artur <artur@example.com>
2024-12-24 19:17:34 +0300

    dictionary

---
+++ dictionary_about_a_car.py
@@ -0,0 +1,21 @@
+corolla_cross = {
+    'information': 'Corolla Cross Гібрид',
+    'cost': '1 658 563 грн',
+    'engine_displacement': '2.0 Hybrid (197 к.с.)',
+    'massa': '1510-1590 кг',
+    'max_speed': '180 км/год',
+    'interior': ['Кермо оздоблене шкірою', 'Фонова ілюмінація передніх дверей','Фонова ілюмінація центральної консолі'],
+    'parameters_baggage': {
+        'volume_of_the_luggage_compartment': '390 л',
+        'volume_of_the_luggage_compartment_with_folded_seats': '1299 л'
+
+    }
+}
+
+corolla_cross['max_massa_trailer_with_brakes'] = '750 кг'
+#max_masa_trailer = corolla_cross['max_massa_trailer_with_brakes']
+
+name_of_car = corolla_cross['information']
+cost_of_car = corolla_cross['cost']
+first_interior_of_car = corolla_cross['interior'][0]
+car_with_folded_seats = corolla_cross['parameters_baggage']['volume_of_the_luggage_compartment_with_folded_seats']
