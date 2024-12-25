d3414f80c5c58b96dbb4f07fbc2a0071c937cb2d
Artur <artur@example.com>
2024-12-25 18:28:44 +0300

    corrected

--- test_utils.py
+++ test_utils.py
@@ -1,15 +1,15 @@
 import utils
 
 
-def test_calculate_area_of_a_rectange():
+def test_calculate_area_of_a_rectange():#intr
     length = 8
     width = 10
-    expected_result = length * width
+    expected_result = 80
     actual_result = utils.calculate_area_of_a_rectange(length=length, width=width)
     assert actual_result == expected_result
 
 
-def test_calculate_area_of_a_rectange_without_length():
+def test_calculate_area_of_a_rectange_without_length():#float
     length = 8
     width = 10
     expected_result = width * width
@@ -17,7 +17,7 @@ def test_calculate_area_of_a_rectange_without_length():
     assert actual_result == expected_result
 
 
-def test_calculate_area_of_a_rectange_without_width():
+def test_calculate_area_of_a_rectange_without_width():#float with intr
     length = 8
     width = 10
     expected_result = length * length
