5b46741cc2241535806c29ab87a01b7307c1b346
Artur <artur@example.com>
2024-12-25 21:19:28 +0300

    modified

--- test_utils.py
+++ test_utils.py
@@ -1,7 +1,7 @@
 import utils
 
 
-def test_calculate_area_of_a_rectange():#intr
+def test_calculate_area_of_a_rectange_with_int_numbers():  # int
     length = 8
     width = 10
     expected_result = 80
@@ -9,17 +9,17 @@ def test_calculate_area_of_a_rectange():#intr
     assert actual_result == expected_result
 
 
-def test_calculate_area_of_a_rectange_without_length():#float
-    length = 8
-    width = 10
-    expected_result = width * width
+def test_calculate_area_of_a_rectange_with_float_numbers():  # float
+    length = 2.5
+    width = 3.4
+    expected_result = 8.5
     actual_result = utils.calculate_area_of_a_rectange(length=length, width=width)
     assert actual_result == expected_result
 
 
-def test_calculate_area_of_a_rectange_without_width():#float with intr
-    length = 8
-    width = 10
-    expected_result = length * length
+def test_calculate_area_of_a_rectange_with_float_and_int_numbers():  # float with int
+    length = 7
+    width = 5.3
+    expected_result = 37.1
     actual_result = utils.calculate_area_of_a_rectange(length=length, width=width)
     assert actual_result == expected_result
--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-def calculate_area_of_a_rectange(length: int | float, width: int | float) -> int | float:
+def calculate_area_of_a_rectange(length: int | float, width: int | float, area_of_rectange=None) -> int | float:
     area_of_rectange = length * width
     return area_of_rectange
