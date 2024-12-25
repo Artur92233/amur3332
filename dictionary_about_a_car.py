3d1101368d2ab21688ce523c77fdc3abbcc923ef
Artur <artur@example.com>
2024-12-25 18:25:42 +0300

    corrected

--- dictionary_about_a_car.py
+++ dictionary_about_a_car.py
@@ -1,10 +1,12 @@
 corolla_cross = {
     'information': 'Corolla Cross Гібрид',
-    'cost': '1 658 563 грн',
+    'cost,grn': 1658563,
     'engine_displacement': '2.0 Hybrid (197 к.с.)',
-    'massa': '1510-1590 кг',
+    'massa,': {'min_massa': 1510,
+               'max_massa': 1590},
     'max_speed': '180 км/год',
-    'interior': ['Кермо оздоблене шкірою', 'Фонова ілюмінація передніх дверей','Фонова ілюмінація центральної консолі'],
+    'interior': ['Кермо оздоблене шкірою', 'Фонова ілюмінація передніх дверей',
+                 'Фонова ілюмінація центральної консолі'],
     'parameters_baggage': {
         'volume_of_the_luggage_compartment': '390 л',
         'volume_of_the_luggage_compartment_with_folded_seats': '1299 л'
@@ -13,9 +15,9 @@ corolla_cross = {
 }
 
 corolla_cross['max_massa_trailer_with_brakes'] = '750 кг'
-#max_masa_trailer = corolla_cross['max_massa_trailer_with_brakes']
+# max_masa_trailer = corolla_cross['max_massa_trailer_with_brakes']
 
 name_of_car = corolla_cross['information']
-cost_of_car = corolla_cross['cost']
+cost_of_car = corolla_cross['cost,grn']
 first_interior_of_car = corolla_cross['interior'][0]
 car_with_folded_seats = corolla_cross['parameters_baggage']['volume_of_the_luggage_compartment_with_folded_seats']
