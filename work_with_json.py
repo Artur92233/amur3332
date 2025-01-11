6862bdb84b2b7b46aa822daa24d0139b6452db0d
Artur <artur@example.com>
2025-01-11 21:43:47 +0300

    json and pdf

--- work_with_json.py
+++ work_with_json.py
@@ -1,7 +1,14 @@
 import json
 import requests
 
-data =
+
+url = 'http://api.open-notify.org/astros.json'
+
+params = {}
+
+response = requests.get(url, params=params)
+response_json = response.json()
+
 
 with open('json_data.json', mode='w', encoding='utf-8') as file:
-    json.dump(data, file, indent=4)
+    json.dump(response_json, file, indent=4)
