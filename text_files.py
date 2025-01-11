d7465ff2b99b4ba289b118a73e285438ed820815
Artur <artur@example.com>
2025-01-11 17:02:53 +0300

    airports

--- text_files.py
+++ text_files.py
@@ -1,4 +1,4 @@
-with open('airport-codes_csv.csv', mode='r',encoding='utf-8') as airports:
+with open('airport-codes_csv.csv', mode='r', encoding='utf-8') as airports:
     airports_data = airports.readlines()
     airports = []
     for row, airport in enumerate(airports_data, start=1):
@@ -8,8 +8,6 @@ with open('airport-codes_csv.csv', mode='r',encoding='utf-8') as airports:
         airport_country = airport_clean[5]
         if airport_country == 'UA':
             airports.append(airport_clean[2])
-
-
 
 result = ', '.join(airports)
 print(result)
