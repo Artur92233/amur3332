021f1535c62dbf2af9818f504a49beed9ea1e573
Artur <artur@example.com>
2025-01-11 16:26:03 +0300

    airports

--- text_files.py
+++ text_files.py
@@ -6,9 +6,11 @@ with open('airport-codes_csv.csv', mode='r',encoding='utf-8') as airports:
             continue
         airport_clean = airport.strip().split(';')
         airport_country = airport_clean[5]
+        if airport[5] == 'UA':
+            airports.append(airport[2])
 
 
 
+        #result = ', '.join(how_many_people_in_iss)
+print(airports)
 
-
-        pass
